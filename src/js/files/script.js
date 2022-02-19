e0578435051150a60a5daa216fadba53242277f3
Nazar Kultayev <nazarcool2001@gmail.com>
2022-02-19 21:06:51 +0500

    Сделано все, кроме шапки

---
+++ src/js/files/script.js
@@ -0,0 +1,24 @@
+// Подключение функционала "Чертогов Фрилансера"
+import { isMobile } from "./functions.js";
+// Подключение списка активных модулей
+import { flsModules } from "./modules.js";
+/*
+window.onload = function () {
+    const containerWidth = 1680;
+    let w = window.innerWidth;
+    let divide = w / containerWidth;
+    const path = document.querySelector('path');
+    if (divide < 1) {
+        path.style.transform = `scale(${divide})`;
+    }
+}
+window.onresize = function () {
+    const containerWidth = 1680;
+    const headerHight = 710;
+    let w = window.innerWidth;
+    let divide = w / containerWidth;
+    const path = document.querySelector('path');
+    if (divide < 1) {
+        path.style.transform = `scale(${divide})`;
+    }
+}*/
