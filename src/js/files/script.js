9d1d64481037591ddf9a3fae7825aa09c06caff5
Nazar Kultayev <nazarcool2001@gmail.com>
2022-02-26 21:33:23 +0500

    Обновлен фон

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -9,20 +9,29 @@ const svgTeam = document.querySelectorAll('.team__card');
 const wrapper = document.querySelector('.wrapper');
 const border_header = document.querySelector('#paint0_linear_28_83');
 const border_header_mobile = document.querySelector('#paint0_linear_28_84');
+const collection = document.querySelector('.collection');
 
 let minus;
 
 document.documentElement.classList.add('loading')
 window.onscroll = function () {
     let scrollTopProcent = window.pageYOffset / document.documentElement.offsetHeight * 100;
+    if (window.pageYOffset >= (collection.offsetTop - 120)) {
+        collection.classList.add('_show');
+    }
+    else {
+        collection.classList.remove('_show');
+    }
 
     if (scrollTopProcent > 100) {
         scrollTopProcent = 100 - (scrollTopProcent - (Math.floor(scrollTopProcent / 100) * 100));
     }
     setParallaxItemsStyle(scrollTopProcent);
+
+
 };
 function setParallaxItemsStyle(scrollTopProcent) {
-    wrapper.style.backgroundPosition = `${scrollTopProcent}%`;
+    wrapper.style.backgroundPosition = ` 0% ${scrollTopProcent}%`;
 }
 window.onload = function () {
     document.documentElement.classList.remove('loading');
@@ -43,25 +52,25 @@ window.onload = function () {
                             fill="#C4C4C4" stroke="#9102D4" stroke-width="2" />
                     </mask>
                     <g mask="url(#mask${i})">
-                        <rect x="-5.4805" y="-3" width="264.62" height="318" fill="#C4C4C4" />
-                        <path
-                            d="M-80.0062 -4.00001C-80.0062 -12.8366 -72.8427 -20 -64.0062 -20H299.304C308.14 -20 315.304 -12.8366 315.304 -4V309H-80.0062V-4.00001Z"
-                            fill="url(#pattern${i})" />
-                        <path d="M198.955 -7.32325L216.212 -7.45068L261.87 30.0366L263.818 46.1151L198.955 -7.32325Z"
-                            fill="black" />
-                        <path d="M6 39.9999L1.00003 33.9999L1.00004 90.9999L6.00003 80.9999L6 39.9999Z" fill="black" />
-                        <path
-                            d="M19.3614 288H119.809L127.91 295.5H260.76L271.021 313L260.76 322.5L249.419 340L211.616 343H34.4826L-1.16016 315L19.3614 288Z"
-                            fill="#0E0D0D" stroke="black" />
-                        <path
-                            d="M1 313.5V14L9.10062 1H222.957L253.199 26V303.5L264 313.5L254.819 322H227.277L211.616 342H34.4825L1 313.5Z"
-                            stroke="black" stroke-width="2" />
+                        <rect x="-5.48047" y="-3" width="264.62" height="318" fill="#C4C4C4"/>
+                        <rect x="-5" y="2" width="260" height="310" fill="black"/>
+                        <rect x="-5" y="2" width="260" height="310" fill="url(#pattern${i})"/>
+                        <rect x="-5" y="2" width="260" height="310" fill="url(#paint0_linear_37_118)"/>
+                        <path d="M198.955 -7.32324L216.212 -7.45067L261.87 30.0366L263.818 46.1151L198.955 -7.32324Z" fill="#111111"/>
+                        <path d="M5.99997 39.9999L1 33.9999L1.00001 90.9999L6 80.9999L5.99997 39.9999Z" fill="#111111"/>
+                        <path d="M19.3614 288H119.809L127.91 295.5H260.76L271.021 313L260.76 322.5L249.419 340L211.616 343H34.4826L-1.16016 315L19.3614 288Z" fill="#0F0F0F" stroke="#111111"/>
+                        <path d="M1 313.5V14L9.10062 1H222.957L253.199 26V303.5L264 313.5L254.819 322H227.277L211.616 342H34.4825L1 313.5Z" stroke="#111111" stroke-width="2"/>
                     </g>
                     <defs>
                         <pattern id="pattern${i}" width="1" height="1">
                             <use xlink:href="#image${i}" />
                         </pattern>
-                        <image  width="155%" height="105%" id="image${i}" xlink:href="img/team/people/${i}.png" />
+                        <linearGradient id="paint0_linear_37_118" x1="125" y1="2" x2="125" y2="312" gradientUnits="userSpaceOnUse">
+                            <stop stop-color="#00114D" stop-opacity="0"/>
+                            <stop offset="0.785219" stop-color="#001254" stop-opacity="0.567708"/>
+                            <stop offset="1" stop-color="#335EF8"/>
+                        </linearGradient>
+                        <image x="-30%" y="-9%"  width="150%" height="105%" id="image${i}" xlink:href="img/team/people/${i}.png" />
                     </defs>
                 </svg>
         `)
